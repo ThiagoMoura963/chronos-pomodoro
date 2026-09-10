961185f68b2683514aadc1861d80ae743ef7b9c7
Thiago Moura <thiagomjfelis3@gmail.com>
2026-09-10 16:44:18 -0300

    feat: compose App with form components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { PlayCircleIcon } from "lucide-react";
 import { Container } from "./components/Container";
-import { CountDown } from "./components/Container/CountDown";
+import { CountDown } from "./components/CountDown";
+import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
+import { DefaultInput } from "./components/DefaultInput";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import "./styles/global.css";
 import "./styles/theme.css";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -18,6 +23,34 @@ function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput
+              id="inputText"
+              type="text"
+              placeholder="Digite algo"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <Cycles />
+          </div>
+
+          <div className="formRow">
+            <DefaultButton icon={<PlayCircleIcon />} />
+          </div>
+        </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
