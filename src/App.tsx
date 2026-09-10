5547c698a1c0832ee98ed945745d4b7c837e9921
Thiago Moura <thiagomjfelis3@gmail.com>
2026-09-10 19:40:37 -0300

    refactor: organize app into pages and templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
-import { Container } from "./components/Container";
-import { CountDown } from "./components/CountDown";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
 import "./styles/global.css";
 import "./styles/theme.css";
-import { Footer } from "./components/Footer";
-import { MainForm } from "./components/MainForm";
+import { Home } from "./pages/Home";
 
 function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <MainForm />
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
