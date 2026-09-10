7a3c49b3b5124caee2a2023507cc92a406151e8e
Thiago Moura <thiagomjfelis3@gmail.com>
2026-09-10 16:43:10 -0300

    feat: add `DefaultButton` component

---
+++ src/components/DefaultButton/index.tsx
@@ -0,0 +1,24 @@
+import styles from "./styles.module.css";
+
+type DefaultButtonProps = {
+  icon: React.ReactNode;
+  color?: "green" | "red";
+} & React.ComponentProps<"button">;
+
+export function DefaultButton({
+  icon,
+  color = "green",
+  ...props
+}: DefaultButtonProps) {
+  return (
+    <>
+      <button
+        className={`${styles.button} ${styles[color]}`}
+        color={color}
+        {...props}
+      >
+        {icon}
+      </button>
+    </>
+  );
+}
